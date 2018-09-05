940de982f993f515e985f0b87336a44175e3a477
dariamikl <d.miklashevskaya@innopolis,ru>
2018-09-05 23:52:05 +0300

    added deletion in 3rd task

--- week3/ex3.c
+++ week3/ex3.c
@@ -15,20 +15,19 @@ struct LinkedListNode *head = NULL;
 
 void print_list();
 
+/* inserts an element with a specific value on a specific place after some element */
 void insert_node(int value, int index) {
-    struct LinkedListNode *new =(struct LinkedListNode*) malloc (sizeof(struct LinkedListNode));
+    struct LinkedListNode *new = (struct LinkedListNode *) malloc(sizeof(struct LinkedListNode));
 
     new->value = value;
-    if (head==NULL){
+    if (head == NULL) {
         new->next = NULL;
         head = new;
-    }
-    else
-    {
-        struct LinkedListNode *temp =(struct LinkedListNode*) malloc (sizeof(struct LinkedListNode));
-        temp  = head;
+    } else {
+        struct LinkedListNode *temp = (struct LinkedListNode *) malloc(sizeof(struct LinkedListNode));
+        temp = head;
         int i = 0;
-        while(i<index) {
+        while (i < index) {
             temp = temp->next;
             i++;
         }
@@ -38,21 +37,41 @@ void insert_node(int value, int index) {
     }
 
     printf("Node inserted at index %d \n", index);
-   print_list();
+    print_list();
 
 }
 
-void print_list(){
-    if (head==NULL){
-        printf("No elements");
+/* delete specific node by its value */
+void delete_node(int value) {
+    struct LinkedListNode *temp = head;
+    while (temp->value != value) {
+        if (temp->next == NULL){
+            printf("Such value was not found\n");
+            return;
+        }
+        else {
+            temp = temp->next;
+
+        }
     }
 
-    else
-    {
+    temp->value = temp->next->value;
+    struct LinkedListNode *next = temp->next->next;
+    free(temp->next);
+    temp->next = next;
+
+    print_list();
+
+}
+
+void print_list() {
+    if (head == NULL) {
+        printf("No elements");
+    } else {
         struct LinkedListNode *temp = head;
         int index = 0;
         printf("The list now looks like this:\n");
-        while(temp->next != NULL){
+        while (temp->next != NULL) {
             printf("%d (%d) ->", temp->value, index);
             temp = temp->next;
             index++;
@@ -62,12 +81,13 @@ void print_list(){
 
     }
 }
+
 int main() {
 
     insert_node(10, 0);
     insert_node(12, 0);
     insert_node(16, 1);
-
+    delete_node(12);
 
 
 }
