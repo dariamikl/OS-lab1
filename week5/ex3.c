5fa59a74e8fc531a7efd42d1a95265b89db1421e
dariamikl <d.miklashevskaya@innopolis,ru>
2018-09-19 21:44:54 +0300

    added third task

--- week5/ex3.c
+++ week5/ex3.c
@@ -1,5 +1,67 @@
 //
 // Created by Daria Miklashevskaya on 19/09/2018.
 //
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFFERSIZE 7
+
+int itemCount = 0;
+pthread_cond_t full, empty;
+
+void* produce(void *arg) {
+    while (1) {
+        if (itemCount == BUFFERSIZE) {
+            printf("Producer sleeps\n");
+            usleep(10000000);
+
+        }
+        itemCount++;
+        printf("Item produced %d\n", itemCount);
+        if (itemCount == 1) {
+            printf("Wake up consumer\n");
+            pthread_cond_signal(&full);
+        }
+    }
+
+}
+
+void* consume(void *arg){
+    int item;
+    while(1){
+        if (itemCount==0){
+            printf("Consumer sleeps\n");
+            usleep(100000000);
+        }
+        itemCount--;
+        printf("Item consumed %d\n", itemCount);
+        if (itemCount==BUFFERSIZE-1)
+            printf("Wake up producer\n");
+            pthread_cond_signal(&empty);
+
+    }
+}
+
+int main(){
 
 
+    pthread_t producer, consumer;
+    pthread_cond_init(&full, 0);
+    pthread_cond_init(&empty, 0);
+
+
+
+    pthread_create(&producer, NULL, produce, NULL);
+    pthread_create(&consumer, NULL, consume, NULL);
+
+    pthread_join(producer, 0);
+    pthread_join(consumer, 0);
+
+    pthread_cond_destroy(&full);
+    pthread_cond_destroy(&empty);
+
+
+
+}
