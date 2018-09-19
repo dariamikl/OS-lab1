5b99154e99947131bcdb54cf8140b3e2c0408cfb
dariamikl <d.miklashevskaya@innopolis,ru>
2018-09-19 19:34:14 +0300

    added first task

--- week5/ex1.c
+++ week5/ex1.c
@@ -1,4 +1,47 @@
 //
 // Created by Daria Miklashevskaya on 19/09/2018.
 //
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_THREAD_NUM 13
+
+void *threadFunc(void *arg){
+    printf("Hello :) I am thread number %d\n",*(int *) arg);
+}
+
+
+int main()
+{
+    pthread_t threads[MAX_THREAD_NUM];
+    parallel_threads(&threads);
+    sequential_threads(&threads);
+
+}
+int parallel_threads(pthread_t threads[]){
+
+    int i;
+    for (int i = 0; i< MAX_THREAD_NUM; i++){
+        pthread_create(&threads[i], NULL, threadFunc, &i);
+        printf("Thread number %d created\n",i );
+
+
+    }
+    for (int i = 0; i< MAX_THREAD_NUM; i++){
+        pthread_join(threads[i], NULL);
+
+    }
+}
+int sequential_threads(pthread_t threads[]){
+    int i;
+    for (int i = 0; i< MAX_THREAD_NUM; i++) {
+        pthread_create(&threads[i], NULL, threadFunc, &i);
+        printf("Thread number %d created\n", i);
+        pthread_join(threads[i], NULL);
+
+
+    }
+}
+
 
---
+++ week5/ex3.c
@@ -0,0 +1,5 @@
+//
+// Created by Daria Miklashevskaya on 19/09/2018.
+//
+
+
