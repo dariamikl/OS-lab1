ecff15e863627d886bc9322a294f76ed04b45d43
dariamikl <d.miklashevskaya@innopolis,ru>
2018-10-25 23:56:07 +0300

    + 4th

--- week10/ex4.c
+++ week10/ex4.c
@@ -3,7 +3,29 @@
 //
 
 #include <sys/stat.h>
+#include <dirent.h>
+#include <stdio.h>
 
 int main(){
-    int stut;
+    DIR *dir;
+    struct dirent *de;
+    struct stat buff;
+    char * curr;
+    long long unsigned p;
+
+    dir = opendir("./tmp");
+
+    while((de=readdir(dir))!=0){
+        curr = de -> d_name;
+        int res = stat(curr,&buff);
+        if (buff.st_ino != p){
+        printf("%d ",buff.st_nlink);
+        printf("%s\n",curr);
+    }
+    }
+
+
+    closedir(dir);
+
+
 }
