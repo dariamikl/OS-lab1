d9ca86ada842dedfb7e28cb73432a0506c79c48a
dariamikl <d.miklashevskaya@innopolis,ru>
2018-11-08 20:27:33 +0300

    +first

---
+++ week12/ex1.c
@@ -0,0 +1,28 @@
+
+
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main() {
+    int fd = open("/dev/random", O_RDONLY);
+
+    char bf[20];
+    read(fd, bf, 20);
+
+    char string[21];
+    int i = 0;
+
+    while (i < 20) {
+        string[i] = 'a' + (int) bf[i] % ('z' - 'a' + 1);
+        i++;
+    }
+
+    string[20] = '\0';
+
+
+    printf("%s\n", string);
+
+}
